c55afe530cdfdcd6cb698a651edba01120d3d89c
gad-restart <gada@restartit.co.il>
2019-08-29 22:56:53 +0300

    add action to reducer, create addContact method in ContactState

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import ContactContext from "../../context/contacts/ContactContext";
+
+const initialContact = {
+  name: "",
+  email: "",
+  phone: "",
+  type: "personal"
+};
 
 const ContactForm = () => {
-  const [contact, setContact] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    type: "personal"
-  });
+  const contactContext = useContext(ContactContext);
+
+  const [contact, setContact] = useState(initialContact);
 
   const { name, email, phone, type } = contact;
 
- const onChange = e => setContact({ ...contact, [e.target.name]: e.target.value });
+  const onChange = e =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact(initialContact);
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className="text-primary">Add Contact</h2>
       <input
         type="text"
@@ -42,6 +54,7 @@ const ContactForm = () => {
         name="type"
         value="personal"
         checked={type === "personal"}
+        onChange={onChange}
       />{" "}
       Personal{" "}
       <input
@@ -49,10 +62,15 @@ const ContactForm = () => {
         name="type"
         value="professional"
         checked={type === "professional"}
+        onChange={onChange}
       />{" "}
       Professional{" "}
       <div>
-          <input type="submit" value="Add Contact" className="btn btn-primary btn-block"/>
+        <input
+          type="submit"
+          value="Add Contact"
+          className="btn btn-primary btn-block"
+        />
       </div>
     </form>
   );
