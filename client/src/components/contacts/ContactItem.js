33a6d2fac8bba4d858056936204508cdfd73fc01
gad-restart <gada@restartit.co.il>
2019-08-29 23:05:41 +0300

    create deleteContact in ContactState, create onDelete in ContactItem component

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import ContactContext from '../../context/contacts/ContactContext';
 import { firstCharUpperCase } from "../../utils/Utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./style.css";
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact } = contactContext;
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+  }
+
   return (
     <div className="card bg-light">
       <h3 className="text-primary text-left">
@@ -32,7 +40,7 @@ const ContactItem = ({ contact }) => {
         )}
         <p>
           <button className="btn btn-dark btn-sm">Edit</button>
-          <button className="btn btn-danger btn-sm">Delete</button>
+          <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
         </p>
       </ul>
     </div>
