58147027b77c180e90b50d8c60ceb802b88e0516
gad-restart <gada@restartit.co.il>
2019-08-29 22:19:02 +0300

    create style.css for contacts

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { firstCharUpperCase } from '../../utils/Utils';
+import './style.css';
+
 const ContactItem = ({ contact }) => {
   const { id, name, email, phone, type } = contact;
   return (
@@ -7,8 +9,8 @@ const ContactItem = ({ contact }) => {
       <h3 className="text-primary text-left">
         {name}{" "}
         <span
-          className={
-            "badge " +
+          className={ 
+            "contact-type badge " +
             (type === "professional" ? "badge-success" : "badge-primary")
           }
         >
