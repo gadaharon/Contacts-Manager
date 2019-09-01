47572e1072c86df1bd7bf208926b597d2929dff5
gad-restart <gada@restartit.co.il>
2019-09-01 21:30:08 +0300

    render dynamic contact according to filtered

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,12 +5,20 @@ import ContactContext from "../../context/contacts/ContactContext";
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem contact={contact} key={contact.id} />
-      ))}
+      {filtered
+        ? filtered.map(contact => (
+            <ContactItem contact={contact} key={contact.id} />
+          ))
+        : contacts.map(contact => (
+            <ContactItem contact={contact} key={contact.id} />
+          ))}
     </Fragment>
   );
 };
