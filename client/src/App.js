0f2fc2a1356d5389e576801d0ee275285efe6969
gad-restart <gada@restartit.co.il>
2019-09-01 22:28:26 +0300

    1) create AuthContext, AuthReducer, AuthState 2) craete Login, Register Pages

--- client/src/App.js
+++ client/src/App.js
@@ -13,22 +13,25 @@ library.add(fab, fas);
 
 /* eslint-disable import/first */
 import ContactState from "./context/contacts/ContactState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
