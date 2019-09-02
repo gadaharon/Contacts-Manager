fb720e0b4378f67b090c795f701aa3014a6389d2
gad-restart <gada@restartit.co.il>
2019-09-02 19:10:08 +0300

    create AlertContext, AlertState, AlertReducer

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
-import Login from './components/auth/Login';
+import Login from "./components/auth/Login";
 import "./App.css";
 
 // Font Awesome React
@@ -16,24 +16,27 @@ library.add(fab, fas);
 /* eslint-disable import/first */
 import ContactState from "./context/contacts/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertContext";
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
